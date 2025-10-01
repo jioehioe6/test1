a011466432215438715f0c2ae82ed6c8fd4e76ed
Abhal Benny <abhalbenny3@gmail.com>
2025-10-01 23:30:41 +0530

    hi

--- backend/api/models/User.js
+++ backend/api/models/User.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true }, // hashed password
-  role: { type: String, default: "subadmin" }  // Add this line
+  role: { type: String, default: "admin" }  // Add this line
 });
 
 module.exports = mongoose.model("User", UserSchema);
